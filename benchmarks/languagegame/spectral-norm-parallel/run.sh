79cf1340c18ff7ac356acb3d772b3cbff11bca2e
dcastrop <d.castro-perez@imperial.ac.uk>
2018-04-12 10:58:06 +0100

    Runtimes with optimised API gen

--- benchmarks/languagegame/spectral-norm-parallel/run.sh
+++ benchmarks/languagegame/spectral-norm-parallel/run.sh
@@ -8,8 +8,8 @@ for i in $LIST; do
   echo > ./$i.time
   for n in 2000 3000 5500 11000; do
     for ncpu in $(seq 1 16); do
-      for iters in $(seq 1 20); do
-        echo "Spectral norm '$i' ($n) ($ncpu) ($iters of 20)"
+      for iters in $(seq 1 30); do
+        echo "Spectral norm '$i' ($n) ($ncpu) ($iters of 30)"
         ./$i/$i -ncpu $ncpu -n $n >> ./$i.time
       done
     done
